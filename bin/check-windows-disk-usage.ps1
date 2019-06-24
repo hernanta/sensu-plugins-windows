05517495e7d9bbe534c33b4d125e75cbb9062746
hernantaa <hernantaa@gmail.com>
2019-06-24 11:48:36 +0700

    fix check disk usage

--- bin/check-windows-disk-usage.ps1
+++ bin/check-windows-disk-usage.ps1
@@ -1,42 +1,27 @@
-$counterValueArray = (Get-counter  -Counter "\LogicalDisk(*)\% Free Space").countersamples | where {$_.InstanceName -ne "_total" -and $_.InstanceName -notmatch "harddiskvolume"} | Sort-Object InstanceName
-$warningValue = 15
-$critialValue = 10
+$freeMemoryDetail = (Get-counter  -Counter "\LogicalDisk(*)\% Free Space").countersamples | where {$_.InstanceName -ne "_total" -and $_.InstanceName -notmatch "harddiskvolume"} | Sort-Object InstanceName
+$freeMemoryPercent = [System.Math]::Round($freeMemoryDetail.CookedValue,2)
+$memoryUsagePercent = 100 - $freeMemoryPercent
+$warningValue = 80
+$critialValue = 90
 $message = ""
 $exitCode = -1
- 
-foreach ($counterValue in $counterValueArray) 
-{ 
-    if ($message -ne "")
-    {
-        $message += "`n"
-    }
-    
-    $message += $counterValue.InstanceName + " " + [System.Math]::Round($counterValue.CookedValue,2) + "% free space"
- 
-    if($counterValue.CookedValue -le $critialValue)
-    {
-        $exitCode = 2
-        $message += " is less than 10%"
-    }
-    elseif ($counterValue.CookedValue -le $warningValue)
-    {
-        if ($exitCode -lt 1)
-        {
-            $exitCode = 1
-        }
 
-        $message += " is less than 15%"
-    }
-    else
-    {
-        if ($exitCode -lt 0)
-        {
-            $exitCode = 0
-        }  
-        
-        $message += " is normal"     
-    }
+
+if ($cpuPercent -gt $criticalValue)
+{
+    $message = "HDD usage is over 90%: $memoryUsagePercent%"
+    $exitCode = 2
 }
- 
+elseif ($cpuPercent -ge $warningValue)
+{
+    $message = "HDD usage is over 80%: $memoryUsagePercent%"
+    $exitCode = 1
+}
+else
+{
+    $message = "HDD is normal: $memoryUsagePercent%"
+    $exitCode = 0
+}
+
 Write-Output $message
 exit $exitCode
