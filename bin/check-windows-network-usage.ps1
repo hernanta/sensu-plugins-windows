74f19fc10b1cad62583657fead318ce8f2c6ecce
hernantaa <hernantaa@gmail.com>
2019-06-24 11:50:10 +0700

    fix check disk usage

--- bin/check-windows-network-usage.ps1
+++ bin/check-windows-network-usage.ps1
@@ -1,16 +1,5 @@
-$ifIndex = Get-WmiObject -Class win32_ip4routetable | where {$_.destination -eq "0.0.0.0"} 
-
-| select -ExpandProperty InterfaceIndex
-$ifIndex = "InterfaceIndex=" + $ifIndex
-$nic_name = Get-WmiObject -Class win32_networkadapterconfiguration -Filter $ifIndex | 
-
-select -ExpandProperty Description
-$nic = [System.Net.NetworkInformation.Networkinterface]::GetAllNetworkInterfaces() | where 
-
-{($_.description -eq $nic_name) -and ($_.operationalstatus -eq "up")}
-$stats = $nic.GetIPv4Statistics()
-$bytesSent = $stats.BytesSent
-$bytesReceived = $stats.BytesReceived
+$bytesReceived = (Get-NetAdapterStatistics -Name Ethernet).ReceivedBytes
+$bytesSent = (Get-NetAdapterStatistics -Name Ethernet).SentBytes
 $networkIn = $bytesReceived/1000000
 $networkOut = $bytesSent/1000000
 $resultNetworkIn = [Math]::Round($networkIn,2)
