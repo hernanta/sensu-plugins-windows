992bb469e2d599ff64938ca0861f19d01dd08ea8
Hernanta Kusuma Cakrawerdaya <hernantaa@gmail.com>
2019-06-24 13:34:22 +0700

    Update check-windows-network-usage.ps1

--- bin/check-windows-network-usage.ps1
+++ bin/check-windows-network-usage.ps1
@@ -4,7 +4,7 @@ $networkIn = $bytesReceived/1000000
 $networkOut = $bytesSent/1000000
 $resultNetworkIn = [Math]::Round($networkIn,2)
 $resultNetworkOut = [Math]::Round($networkOut,2)
-$exitCode = -1
+$exitCode = 0
 
 Write-Output "Bandwidth - Incoming: $resultNetworkIn MByte" 
 Write-Output "Bandwidth - Outgoing: $resultNetworkOut MByte"
